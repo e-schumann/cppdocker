15daa4e47c2f931d5cb50e4e501a7314e19211f2
eschumann <e-schumann@online.de>
2018-05-03 10:11:20 +0200

    Buck tests added

---
+++ tests/buck/demo/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <mathutils/add.hpp>
+
+int main() {
+  std::cout << "Hello, my world. " << std::endl;
+  std::cout << "3 + 5 = " << add(3, 5) << std::endl;
+  return 0;
+}
